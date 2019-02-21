72407d49a832e571b2b884801b8d321743b3a3d6
Michael Nguyen <michaelnguyen974@gmail.com>
2019-02-21 18:29:48 +0000

    test RED: first test written, roll function not found

--- tests/Scorecard.test.js
+++ tests/Scorecard.test.js
@@ -1,9 +1,13 @@
-const Scorecard = require('../src/Scorecard.js/index.js');
+const { Scorecard } = require('../src/Scorecard.js');
 
 describe("Scorecard", () => {
 
-  beforeEach("scorecard", () => {
+  beforeEach( () => {
     scorecard = new Scorecard();
-  })
+  });
+
+  test("scorecard returns the roll from user input", () => {
+    expect(scorecard.roll(4,0)).toEqual(4);
+  });
 
 });
