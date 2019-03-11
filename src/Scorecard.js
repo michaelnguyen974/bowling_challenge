f28758c99eb43e824a3ab1c03da65eb3754b23c4
Michael Nguyen <michaelnguyen974@gmail.com>
2019-03-11 11:21:00 +0000

    test GREEN: score function now calculates rolls, however rolls needs to be refactored

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,12 +1,16 @@
 class Scorecard {
-
+  constructor() {
+    this.frames = []
+  }
 
   roll(roll1, roll2) {
     return roll1 + roll2
   }
 
   score() {
-    
+    return this.frames.reduce(function(score, frame, frameindex, frames) {
+      return score + frame.total(frames[frameindex +1], frames[frameindex + 2])
+    }, 0)
   }
     
 };
