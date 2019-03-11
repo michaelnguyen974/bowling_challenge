726ec3865c2d376bfad881b09f7e9574a974aae0
Michael Nguyen <michaelnguyen974@gmail.com>
2019-03-11 11:17:18 +0000

    update test: Added generate new Frame function to tests

--- tests/Scorecard.test.js
+++ tests/Scorecard.test.js
@@ -6,8 +6,19 @@ describe('Scorecard', () => {
     scorecard = new Scorecard();
   });
 
-  test('scorecard returns the roll from user input', () => {
-    expect(scorecard.roll(4,0)).toEqual(4);
-  });
+  describe("Gutter Game", () => {
+    test('calculates gutter game', () => {
+      generateNewFrame([0, 0])
+      expect(scorecard.score()).toEqual(0);
+    });
+
+  })
+
+  function generateNewFrame(frame, final_frame) {
+    for(i = 0; i < 9; i++) {
+      scorecard.roll(frame);
+    }
+    scorecard.roll(final_frame || frame);
+  }
 
 });
